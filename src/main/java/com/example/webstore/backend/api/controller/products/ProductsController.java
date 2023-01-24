4b229bbc1ec4739b2a31934e8715b974981c6a6f
Nedyalko <102542299+nedK01@users.noreply.github.com>
2023-01-24 11:46:22 +0200

    update

--- src/main/java/com/example/webstore/backend/api/controller/products/ProductsController.java
+++ src/main/java/com/example/webstore/backend/api/controller/products/ProductsController.java
@@ -1,11 +1,7 @@
 package com.example.webstore.backend.api.controller.products;
 
-import com.example.webstore.backend.api.model.UpdateProductRequest;
-import com.example.webstore.backend.api.model.UpdateProductResponse;
 import com.example.webstore.backend.model.Product;
 import com.example.webstore.backend.service.ProductService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,16 +21,7 @@ public class ProductsController {
         return productService.getProducts();
     }
 
-    @PutMapping(value = "/{id}")
-    public ResponseEntity<UpdateProductResponse> updateProduct(@PathVariable Long id, UpdateProductRequest request) {
-
-        Product product = productService.update(id, request);
-
-        UpdateProductResponse productResponse = productService.converter(product);
-
-        return new ResponseEntity<>(productResponse, HttpStatus.OK);
 
 
 
     }
-}
--- src/main/java/com/example/webstore/backend/service/ProductService.java
+++ src/main/java/com/example/webstore/backend/service/ProductService.java
@@ -1,13 +1,10 @@
 package com.example.webstore.backend.service;
 
-import com.example.webstore.backend.api.model.UpdateProductRequest;
-import com.example.webstore.backend.api.model.UpdateProductResponse;
 import com.example.webstore.backend.model.Product;
 import com.example.webstore.backend.model.dto.ProductDAO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -23,37 +20,5 @@ public class ProductService {
     }
 
 
-    public Product update(Product product, UpdateProductRequest request) {
-
-        product.setName(request.getName());
-        product.setShortDescription(request.getShortDescription());
-        product.setLongDescription(request.getLongDescription());
-        product.setPrice(request.getPrice());
-        product.setInventory(request.getInventory());
-
-        return product;
 
     }
-
-    public Product update(Long id, UpdateProductRequest request) {
-
-        Product product = productDAO.findProductById(id);
-
-        Product updatedProduct = update(product, request);
-
-        productDAO.save(updatedProduct);
-
-        return updatedProduct;
-    }
-
-    public UpdateProductResponse converter(Product product) {
-
-        UpdateProductResponse response = new UpdateProductResponse();
-        response.setName(product.getName());
-        response.setShortDescription(product.getShortDescription());
-        response.setLongDescription(product.getLongDescription());
-        response.setPrice(product.getPrice());
-        response.setInventory(product.getInventory());
-        return response;
-    }
-}
