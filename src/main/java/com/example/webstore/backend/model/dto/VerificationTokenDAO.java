a9ebb3f9664fe0a119be792bcf41e0837a1068d1
Nedyalko <102542299+nedK01@users.noreply.github.com>
2023-01-27 13:53:52 +0200

    unit-tests

--- src/main/java/com/example/webstore/backend/model/dto/VerificationTokenDAO.java
+++ src/main/java/com/example/webstore/backend/model/dto/VerificationTokenDAO.java
@@ -4,12 +4,17 @@ import com.example.webstore.backend.model.LocalUser;
 import com.example.webstore.backend.model.VerificationToken;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VerificationTokenDAO extends ListCrudRepository<VerificationToken, Long> {
     Optional<VerificationToken> findByToken(String token);
 
     void deleteByUser(LocalUser user);
+
+    List<VerificationToken> findByUser_IdOrderByIdDesc(Long id);
+
+
 
 
 }
--- src/test/java/com/example/webstore/backend/service/UserServiceTest.java
+++ src/test/java/com/example/webstore/backend/service/UserServiceTest.java
@@ -1,7 +1,12 @@
 package com.example.webstore.backend.service;
 
+import com.example.webstore.backend.api.model.LoginBody;
 import com.example.webstore.backend.api.model.RegistrationRequest;
+import com.example.webstore.backend.exception.EmailFailureException;
 import com.example.webstore.backend.exception.UserAlreadyExistsException;
+import com.example.webstore.backend.exception.UserNotVerifiedException;
+import com.example.webstore.backend.model.VerificationToken;
+import com.example.webstore.backend.model.dto.VerificationTokenDAO;
 import com.icegreen.greenmail.configuration.GreenMailConfiguration;
 import com.icegreen.greenmail.junit5.GreenMailExtension;
 import com.icegreen.greenmail.util.ServerSetupTest;
@@ -15,6 +20,8 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class UserServiceTest {
 
@@ -25,6 +32,8 @@ public class UserServiceTest {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private VerificationTokenDAO verificationTokenDAO;
 
     @Test
     @Transactional
@@ -37,20 +46,72 @@ public class UserServiceTest {
         body.setLastName("LastName");
         body.setPassword("MySecretPassword123");
 
-        Assertions.assertThrows(UserAlreadyExistsException.class, () -> userService.registerUser(body)
-                , "Username should already be in use.");
+        Assertions.assertThrows(UserAlreadyExistsException.class,
+                () -> userService.registerUser(body), "Username should already be in use.");
 
         body.setUsername("UserServiceTest$testRegisterUser");
         body.setEmail("UserA@junit.com");
 
-        Assertions.assertThrows(UserAlreadyExistsException.class, () -> userService.registerUser(body)
+        Assertions.assertThrows(UserAlreadyExistsException.class,
+                () -> userService.registerUser(body)
                 , "Email should already be in use.");
 
         body.setEmail("UserServiceTest$testRegisterUser@junit.com");
-        Assertions.assertDoesNotThrow(() -> userService.registerUser(body)
-                , "User should register successfully.");
+        Assertions.assertDoesNotThrow(() -> userService.registerUser(body), "User should register successfully.");
 
         Assertions.assertEquals(body.getEmail(), greenMailExtension.getReceivedMessages()[0]
                 .getRecipients(Message.RecipientType.TO)[0].toString());
+    }
+
+    @Test
+    @Transactional
+    public void testLoginUser() throws UserNotVerifiedException, EmailFailureException {
+
+        LoginBody body = new LoginBody();
+
+        body.setUsername("UserA-NotExists");
+        body.setPassword("PasswordA123-Bad password");
+        Assertions.assertNull(userService.loginUser(body), "The user should nto exists");
+
+        body.setUsername("UserA");
+        Assertions.assertNull(userService.loginUser(body), "The password should be incorrect");
+
+        body.setPassword("PasswordA123");
+        Assertions.assertNotNull(userService.loginUser(body), "The user should login successfully");
+
+        body.setUsername("UserB");
+        body.setPassword("PasswordB123");
+
+        try {
+            userService.loginUser(body);
+            Assertions.assertTrue(false, "The user should not email verified");
+        } catch (UserNotVerifiedException ex) {
+            Assertions.assertTrue(ex.isNewEmailSend(), ("Email verification should be send"));
+            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
+        }
+        try {
+            userService.loginUser(body);
+            Assertions.assertTrue(false, "The user should not email verified");
+        } catch (UserNotVerifiedException ex) {
+            Assertions.assertFalse(ex.isNewEmailSend(), ("Email verification should not be resend"));
+            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
+        }
+    }
+    @Test
+    @Transactional
+    public void verifyUser() throws EmailFailureException {
+        Assertions.assertFalse(userService.verifyUser("Bad Token"), "Token is bad or does not exist should return false");
+        LoginBody body = new LoginBody();
+        body.setUsername("UserB");
+        body.setPassword("PasswordB123");
+        try {
+            userService.loginUser(body);
+            Assertions.assertTrue(false, "The user should not email verified");
+        } catch (UserNotVerifiedException ex) {
+            List<VerificationToken> tokens = verificationTokenDAO.findByUser_IdOrderByIdDesc(2L);
+            String token = tokens.get(0).getToken();
+            Assertions.assertTrue(userService.verifyUser(token), "Token should be valid");
+            Assertions.assertNotNull(body, "The user should now be verified");
+        }
     }
 }
